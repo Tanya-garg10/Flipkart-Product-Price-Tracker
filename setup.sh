350ae75e9b4b43e66538e4c70c36cefbe663b175
Tanya-garg10 <tanyagarg5315@example.com>
2025-06-23 11:21:00 +0530

    Fix deployment script for Render

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-apt-get update
-apt-get install -y chromium-browser chromium-driver
-
-export CHROME_BIN=/usr/bin/chromium-browser
-export PATH=$PATH:/usr/bin
+pip install -r requirements.txt
+streamlit run app.py
